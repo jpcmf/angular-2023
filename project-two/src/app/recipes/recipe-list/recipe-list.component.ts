ce5a6ad56aa27446f9002bac95a0db18f49b1617
João Paulo Fricks <jpfricks@gmail.com>
2023-03-03 09:55:56 -0300

    Handle with recipe list 🍩

--- project-two/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-two/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a simply description',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
