5666ec07faee54683115c5e6a00d22b31b109ef8
João Paulo Fricks <jpfricks@gmail.com>
2023-03-13 09:30:41 -0300

    Pass recipe data event and property binding to show recipe details 🍩

--- project-two/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-two/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 
 @Component({
@@ -9,18 +9,24 @@ import { Recipe } from '../models/recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
+      'A test recipe 1',
       'This is a simply description',
       'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     ),
     new Recipe(
-      'A test recipe',
+      'A test recipe 2',
       'This is a simply description',
       'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
     ),
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe, index: number) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
