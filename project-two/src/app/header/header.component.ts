212f4544fcffc3cf9ebade80cc199749de9ce765
João Paulo Fricks <jpfricks@gmail.com>
2023-03-18 23:27:15 -0300

    Add dropdown directive to the header and recipe detail 🍩

--- project-two/src/app/header/header.component.ts
+++ project-two/src/app/header/header.component.ts
@@ -1,14 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  visibilityClasses: {};
+  private isVisible: boolean = false;
+
   @Output() featureSelected = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setVisibilityClasses();
+  }
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
+  }
+
+  toggleVisible(isVisible: boolean): void {
+    this.isVisible = isVisible;
+    this.setVisibilityClasses();
+  }
+
+  private setVisibilityClasses(): void {
+    this.visibilityClasses = { hidden: !this.isVisible, '': this.isVisible };
   }
 }
--- project-two/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ project-two/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 
 @Component({
@@ -6,6 +6,24 @@ import { Recipe } from '../models/recipe.model';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
+  visibilityClasses: {};
+  private isVisible: boolean = false;
+
   @Input() recipe: Recipe;
+
+  contructor() {}
+
+  ngOnInit(): void {
+    this.setVisibilityClasses();
+  }
+
+  toggleVisible(isVisible: boolean): void {
+    this.isVisible = isVisible;
+    this.setVisibilityClasses();
+  }
+
+  private setVisibilityClasses(): void {
+    this.visibilityClasses = { hidden: !this.isVisible, '': this.isVisible };
+  }
 }
