abb878ad31332d58ae98823384896493ca133d45
João Paulo Fricks <jpfricks@gmail.com>
2023-03-19 09:07:10 -0300

    Add ingrediens and use services to push data 🍩

--- project-two/src/app/recipes/recipe.service.ts
+++ project-two/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shopping-list/models/ingredient.model';
 import { Recipe } from './models/recipe.model';
 
 @Injectable({
@@ -9,14 +10,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe 1',
-      'This is a simply description',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'Tasty Schnitzel',
+      'Super-tasty Schnitzel - This is a awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A test recipe 2',
       'This is a simply description',
-      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
