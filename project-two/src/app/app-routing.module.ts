a4df6422bd515a9dcc0b653ae039b6d97672c1b5
João Paulo Fricks <jpfricks@gmail.com>
2023-03-27 09:01:37 -0300

    Add the initial routes of the app 🍩

--- project-two/src/app/app-routing.module.ts
+++ project-two/src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
