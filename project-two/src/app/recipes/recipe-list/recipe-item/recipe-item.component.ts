49f41d9808ac11a8381b287ae8a2010bb0079309
João Paulo Fricks <jpfricks@gmail.com>
2023-03-27 23:32:22 -0300

    Remove old logic 🌚

--- project-two/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ project-two/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../models/recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,10 +8,4 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent {
   @Input() recipe: Recipe;
-
-  constructor(private _recipeService: RecipeService) {}
-
-  onSelectedItem() {
-    this._recipeService.recipeSelected.emit(this.recipe);
-  }
 }
