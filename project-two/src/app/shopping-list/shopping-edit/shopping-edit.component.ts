08ea6e8b9975c754a5810173a14b91ec0d8a7a85
João Paulo Fricks <jpfricks@gmail.com>
2023-04-06 23:45:35 -0300

    Clear item in the shopping edit form 🍩

--- project-two/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ project-two/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,4 +53,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
     form.reset();
   }
+
+  onClear() {
+    this.shoppingListForm.reset();
+    this.editMode = false;
+  }
 }
