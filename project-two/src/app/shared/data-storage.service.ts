4a2a8fc42d59363381f4c9eb1b655eb2faaa38a0
João Paulo Fricks <jpfricks@gmail.com>
2023-04-10 19:39:21 -0300

    Create data storage service to handle with http request 🍩

---
+++ project-two/src/app/shared/data-storage.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataStorageService {
+  constructor(
+    private _http: HttpClient,
+    private _recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this._recipeService.getRecipes();
+
+    const url =
+      'https://ng-course-recipe-book-237e4-default-rtdb.firebaseio.com/recipes.json';
+
+    return this._http.put(url, recipes);
+  }
+}
