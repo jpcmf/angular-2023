764bded2dca5be3c43065a0128094ae4ee0ec94e
João Paulo Fricks <jpfricks@gmail.com>
2023-04-11 08:35:11 -0300

    Add recipe service in resolver to handle with edit recipe item 🌚

--- project-two/src/app/recipes/recipes-resolver.service.ts
+++ project-two/src/app/recipes/recipes-resolver.service.ts
@@ -8,18 +8,27 @@ import {
 
 import { Recipe } from './models/recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private _dataStorageSerivce: DataStorageService) {}
+  constructor(
+    private _dataStorageSerivce: DataStorageService,
+    private _recipesService: RecipeService
+  ) {}
 
   // the Angular will subscribe with resolver
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this._dataStorageSerivce.fetchRecipes();
+    const recipes = this._recipesService.getRecipes();
+    if (recipes.length === 0) {
+      return this._dataStorageSerivce.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
