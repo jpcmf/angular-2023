e03b7972869bd9cfb21ed281965b54855c1837ae
João Paulo Fricks <jpfricks@gmail.com>
2023-04-12 19:38:50 -0300

    Handle with auth form 🍩

--- project-two/src/app/auth/auth.component.ts
+++ project-two/src/app/auth/auth.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {
-  onFunc(): void {}
+export class AuthComponent implements OnInit {
+  isLoginMode: boolean = true;
+  authForm: FormGroup;
+
+  ngOnInit(): void {
+    this.authForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  onSwithMode(): void {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(): void {
+    console.log(this.authForm.value);
+  }
 }
