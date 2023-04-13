b47a118fbc755bc988e63fe074f2fd29099a7227
João Paulo Fricks <jpfricks@gmail.com>
2023-04-12 21:15:44 -0300

    Add setenv script to handle with sensitive variables 🌚

---
+++ project-two/scripts/setenv.ts
@@ -0,0 +1,31 @@
+const { writeFile } = require('fs');
+const { argv } = require('yargs');
+
+require('dotenv').config();
+
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+if (!process.env.API_KEY) {
+  console.error('All the required environment variables were not provided!');
+  process.exit(-1);
+}
+
+const targetPath = isProduction
+  ? `./src/environments/environment.prod.ts`
+  : `./src/environments/environment.ts`;
+
+const environmentFileContent = `
+export const environment = {
+   production: ${isProduction},
+   apiKey: "${process.env.API_KEY}"
+};
+`;
+
+writeFile(targetPath, environmentFileContent, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Wrote variables to ${targetPath}`);
+});
