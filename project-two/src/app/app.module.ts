8571bee57b90d1b2ebd1f21a7900630d9e98d55b
João Paulo Fricks <jpfricks@gmail.com>
2023-04-16 00:45:54 -0300

    Add core module to handle with providers 🍩

--- project-two/src/app/app.module.ts
+++ project-two/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
 
+import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-
-import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
@@ -25,9 +24,7 @@ import { AuthInterceptor } from './auth/auth.interceptor';
     ShoppingListModule,
     AppRoutingModule, // This it is important that AppRoutingModule is the last imported one if it contains a wildcard route.
     SharedModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
