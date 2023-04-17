cfc6b287121dea3d34e67085cd65b25f3014cf28
João Paulo Fricks <jpfricks@gmail.com>
2023-04-17 09:58:57 -0300

    Handle routes with standalone components 🍩

--- project-three/src/main.ts
+++ project-three/src/main.ts
@@ -1,4 +1,9 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent);
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(AppRoutingModule)],
+});
