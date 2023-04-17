bc47502dd989be60532cb1df439915cbd091f402
João Paulo Fricks <jpfricks@gmail.com>
2023-04-17 09:23:14 -0300

    Change the root component to standalone 🍩
    - Need change the main.ts to use the bootstrapApplication
    - Need add as import the welcome component
    - Don't need the app module anymore !!!

--- project-three/src/app/app.module.ts
+++ project-three/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { DetailsComponent } from './welcome/details/details.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+// import { AppComponent } from './app.component';
+// import { SharedModule } from './shared/shared.module';
+// import { DetailsComponent } from './welcome/details/details.component';
+// import { WelcomeComponent } from './welcome/welcome.component';
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, WelcomeComponent],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+// @NgModule({
+//   declarations: [],
+//   imports: [BrowserModule, WelcomeComponent],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
